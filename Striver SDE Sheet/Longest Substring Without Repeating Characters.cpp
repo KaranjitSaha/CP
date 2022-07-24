f3dd1066f69cb91b5b48f1a1bae2125b6daeb225
Karanjit Saha <karanjitsaha@pop-os.localdomain>
2022-07-24 20:03:38 +0530

    24July 2022

--- Striver SDE Sheet/Longest Substring Without Repeating Characters.cpp
+++ Striver SDE Sheet/Longest Substring Without Repeating Characters.cpp
@@ -1,3 +1,28 @@
+/* https://leetcode.com/problems/longest-substring-without-repeating-characters/ */
+
+
+/* METHOD 2 */
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int start=0;
+        int ans=0;
+        set<int> charSet;
+        for(int end=0;end<s.size();end++){
+                while(charSet.find(s[end])!=charSet.end()){
+                    charSet.erase(s[start]);
+                    start++;
+                }
+                charSet.insert(s[end]);
+            ans=max(ans,end-start+1);
+        }
+        return ans;
+    }
+};
+
+
+/* METHOD1 */
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
